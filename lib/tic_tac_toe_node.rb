fbe006c57c26f9cde3d62d7af3397e6decd5ba2a
Elliot Kuok <elliotckuok@gmail.com>
2023-06-12 14:58:42 -0700

    Fixing losing_node and winning_node

--- lib/tic_tac_toe_node.rb
+++ lib/tic_tac_toe_node.rb
@@ -9,9 +9,55 @@ class TicTacToeNode
   end
 
   def losing_node?(evaluator)
+    #if game is over, there are 3 scenarios: win, lose, tie
+    if @board.over? 
+      if @board.winner == evaluator
+        return false
+      elsif @board.winner != evaluator
+        return true
+      elsif @board.winner.nil? 
+        return false
+      end
+    end
+    #if evaluator is next this is a losing node if all children are losing nodes
+    if evaluator == @next_mover_mark
+      all_losing_nodes = self.children.all? do |child|
+        child.losing_node?(evaluator)
+      end
+      no_winning_nodes = self.children.none? do |child|
+        child.winning_node?(evaluator)
+      end
+
+      all_losing_nodes || no_winning_nodes
+    # opponent is next; this is a losing node if 
+    else
+      opponent_wins = self.children.any? do |child|
+        child.winning_node?(next_mover_mark)
+      end
+      opponent_wins
+    end
   end
 
-  def winning_node?(evaluator)
+  def winning_node?(evaluator)#this needs to be recursive
+    if @board.over?
+      if @board.winner == evaluator
+        return true
+      elsif @board.winner != evaluator
+        return false
+      elsif @board.winner.nil? 
+        return false
+      end
+    end
+
+    if evaluator == @next_mover_mark
+      next_move_win = self.children.any? do |child|
+        child.winning_node?(evaluator)
+      end
+    else
+      next_move_win = self.children.all? do |child|
+        child.winning_node?(evaluator)
+      end
+    end
   end
 
   # This method generates an array of all moves that can be made after
